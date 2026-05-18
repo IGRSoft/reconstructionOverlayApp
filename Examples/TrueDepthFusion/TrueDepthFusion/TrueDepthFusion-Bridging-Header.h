084049d5f4b26392bc5f386b8bd1e86ed0484bb0
Vitalii Parovishnyk <870237+ikorich@users.noreply.github.com>
2026-05-18 12:13:40 +0300

    feat(example): port SCPointCloudRenderer to Swift; delete bridging header (AC-9)

--- Examples/TrueDepthFusion/TrueDepthFusion/TrueDepthFusion-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "SCPointCloudRenderer.h"
-
-#import <SceneKit/ModelIO.h>
-
-#import <StandardCyborgFusion/SCPointCloud+SceneKit.h>
